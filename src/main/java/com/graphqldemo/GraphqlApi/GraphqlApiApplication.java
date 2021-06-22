de32c41bf1aa9e2ffd0a4767ad06a7e71743333b
a.l.bandyopadhyay <a.l.bandyopadhyay@accenture.com>
2021-06-22 12:16:57 +0530

    initial commit

---
+++ src/main/java/com/graphqldemo/GraphqlApi/GraphqlApiApplication.java
@@ -0,0 +1,13 @@
+package com.graphqldemo.GraphqlApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlApiApplication.class, args);
+	}
+
+}
